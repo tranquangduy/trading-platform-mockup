19f3bb5314950f20a756f86c03160d511e3a4cb6
Quang Duy Tran <hello.itctran@gmail.com>
2024-02-23 17:13:19 +0100

    feat: added button

---
+++ components/atoms/Button.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import React from 'react';
+
+type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
+
+const Button: React.FC<ButtonProps> = ({ children, ...btnProps }) => {
+  return (
+    <button
+      type="submit"
+      className={
+        'flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+      }
+      {...btnProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
